2178816d22126380603dae05a53b08cffc3c9b69
Sumit112234 <sumitbaghel22a@gmail.com>
2025-01-28 19:25:32 +0530

    Grand Finale1

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "https://whatsapp-pre.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
