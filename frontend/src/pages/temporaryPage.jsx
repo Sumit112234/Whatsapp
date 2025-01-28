d27f3d0bae168042e89f914a470ef87ea98c207b
Sumit112234 <sumitbaghel22a@gmail.com>
2025-01-28 15:31:08 +0530

    5

--- frontend/src/pages/temporaryPage.jsx
+++ frontend/src/pages/temporaryPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-
-const socket = io('http://localhost:7896');
+let realBackendUrl = import.meta.env.VITE_API_BACKEND_URL;
+const socket = io(realBackendUrl);
 
 
 const Temp = () => {
