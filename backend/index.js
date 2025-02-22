00d43f7a5822e712c538774a687bcf71451a62de
Sumit112234 <sumitbaghel22a@gmail.com>
2025-02-22 19:34:06 +0530

    backend changed

--- backend/index.js
+++ backend/index.js
@@ -38,9 +38,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 // API Endpoints
-// app.get("/", (req, res) => {
-//   res.json({ message: `Server is running on port ${process.env.PORT}` });
-// });
+app.get("/", (req, res) => {
+  res.json({ message: `Server is running on port ${process.env.PORT}` });
+});
 
 app.get("/api", (req, res) => {
   res.json({ message: `Server API running on port ${process.env.PORT}` });
@@ -73,7 +73,7 @@ connectDb()
   .then(() => {
     const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => {
-      //console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
